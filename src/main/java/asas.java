486c44ceadce345bfdbab15ae20b119dd0c68e72
Vladislav Kolyagin <vladislavkolyagin@yandex.ru>
2022-11-12 16:43:12 +0300

    Parametrizied test

---
+++ src/main/java/asas.java
@@ -0,0 +1,36 @@
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {18,true},
+                {19,true},
+                {20,true},
+                {21,true},
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        // Передай сюда возраст пользователя
+        boolean isAdult = program.checkIsAdult(age);
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("fdfd",result, isAdult);
+    }
+}
